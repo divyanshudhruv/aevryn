a3915110cbc680c476cb3896d2c1a4f266eb8e3c
Divyanshu Dhruv <71079602+divyanshudhruv@users.noreply.github.com>
2026-09-08 13:50:31 +0530

    chore(scaffold): initialize project with Better-T-Stack

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,27 @@
+import { createDb } from "@aevryn/db";
+import * as schema from "@aevryn/db/schema/auth";
+import { env } from "@aevryn/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.BETTER_AUTH_URL],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    plugins: [nextCookies()],
+  });
+}
+
+export const auth = createAuth();
